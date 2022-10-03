0b49092d5ef162e272dd27ab8dc4fd4bf52bc30b
Marcos Paiva <marcospaivajr7@gmail.com>
2022-10-03 15:32:20 -0300

    integrando backend com API Django REST

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class List(models.Model):
-    owener = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
 
     def __str__(self):
