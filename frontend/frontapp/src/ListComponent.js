d59534efa899dbc014eb11a54b5d43edd6449458
marcos7junior <marcospaivajr7@gmail.com>
2022-10-04 20:19:36 -0300

    Criado componente react Item

--- frontend/frontapp/src/ListComponent.js
+++ frontend/frontapp/src/ListComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import ItemComponent from './ItemComponent';
 
 export default function ListComponent() {
     return (
         <div>
             <h2>Minha lista</h2>
             <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
+                <ItemComponent name={'Meu item 1'} />
+                <ItemComponent name={'Meu item 2'} />
             </ul>
         </div>
     );
